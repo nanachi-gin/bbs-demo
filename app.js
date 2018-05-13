612afc20fcd92b85745e40b10350faa8c5cc98d0
PakiP <q321814@outlook.com>
2018-05-13 22:32:34 +0800

    update

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/todo');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test',function (req, res, next) {
+   res.render('index', { title: 'testPage'});
+});
+
 
 module.exports = router;
